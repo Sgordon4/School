ce0ab1f4a9fef7865d9ced1bc6ee2c38340ff5d4
Sgordon4 <SeanGordonkh@gmail.com>
2019-09-17 17:08:58 -0500

    Completed CprE 342 HW2 :D

--- 5th_Semester/CprE_342/Homework/Homework2/tutorial/arithlang/src/main/java/arithlang/Evaluator.java
+++ 5th_Semester/CprE_342/Homework/Homework2/tutorial/arithlang/src/main/java/arithlang/Evaluator.java
@@ -22,31 +22,7 @@ public class Evaluator implements Visitor<Value> {
 
     @Override
     public Value visit(AddExp e) {
-        System.out.println("Adding");
-
-        List<Exp> operands = e.all();
-        int length = operands.size();
-
-        if(length != 2) {
-            System.out.println("Only two operators allowed");
-            return new DynamicError(ts.visit(e));
-        }
-
-        /*
-        char result = 0;
-        for(Exp exp: operands) {
-            CharVal intermediate = (CharVal) exp.accept(this); // Dynamic type-checking
-            result += intermediate.v(); //Semantics of AddExp in terms of the target language.
-        }
-        return new CharVal(result);
-
-         */
-        return new CharVal('a');
-    }
-
-    @Override
-    public Value visit(MultExp e) {
-        System.out.println("Multiplying");
+        //System.out.println("Adding");
 
         List<Exp> operands = e.all();
         int length = operands.size();
@@ -65,23 +41,69 @@ public class Evaluator implements Visitor<Value> {
 
 
         char result;
+        //Using ascii value combinations to determine output...
+        //Ascii values for chars:
+        //e - 101
+        //o - 111
+        //u - 117
+
+        int ascii = (int)char1 + (int)char2;
+
         //Switch case that implements the rules outlined in the HW2 PDF
-        switch(char1){
-            case 'e': 
+        switch(ascii){
+            case 202: result = 'e'; //(* e e)
+                break;
+            case 212: result = 'o'; //(* e o) or (* o e)
+                break;
+            case 222: result = 'e'; //(* o o)
+                break;
+            default:  result = 'u'; //One operator is 'u'
         }
 
-        System.out.println(char1);
-        System.out.println(char2);
-
-        /*
-        char result = 0;
-        for(Exp exp: operands) {
-            CharVal intermediate = (CharVal) exp.accept(this); // Dynamic type-checking
-            result *= intermediate.v(); //Semantics of MultExp.
-        }
         return new CharVal(result);
-         */
-        return new CharVal('m');
+    }
+
+    @Override
+    public Value visit(MultExp e) {
+        //System.out.println("Multiplying");
+
+        List<Exp> operands = e.all();
+        int length = operands.size();
+
+        if(length != 2) {
+            System.out.println("Only two operators allowed");
+            return new DynamicError(ts.visit(e));
+        }
+
+
+        CharVal val1 = (CharVal) operands.get(0).accept(this);
+        char char1 = val1.v();
+        CharVal val2 = (CharVal) operands.get(1).accept(this);
+        char char2 = val2.v();
+
+
+
+        char result;
+        //Using ascii value combinations to determine output...
+        //Ascii values for chars:
+        //e - 101
+        //o - 111
+        //u - 117
+
+        int ascii = (int)char1 + (int)char2;
+
+        //Switch case that implements the rules outlined in the HW2 PDF
+        switch(ascii){
+            case 202: result = 'e'; //(* e e)
+                break;
+            case 212: result = 'e'; //(* e o) or (* o e)
+                break;
+            case 222: result = 'o'; //(* o o)
+                break;
+            default:  result = 'u'; //One operator is 'u'
+        }
+
+        return new CharVal(result);
     }
 
     @Override
