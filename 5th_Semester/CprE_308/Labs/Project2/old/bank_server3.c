25c5b8691d3302531134f357745dfca5878fc7c0
Sgordon4 <SeanGordonkh@gmail.com>
2019-11-13 16:40:25 -0600

    Coninuing proj2

--- 5th_Semester/CprE_308/Labs/Project2/old/bank_server3.c
+++ 5th_Semester/CprE_308/Labs/Project2/old/bank_server3.c
@@ -10,7 +10,9 @@ typedef enum{
     TRANSJOB = 2
 } JobType;
 
-
+struct TailNode {
+    struct Job* prev;
+};
 struct CheckJob {
     int account_ID;
 };
@@ -25,12 +27,13 @@ struct TransJob {
 
 struct Job {
     int request_ID;
-    JobType job_type;           //Is this job a CheckJob, TransJob, QueueHead?
+    JobType job_type;           //Is this job a CheckJob, TransJob, ...?
     struct timeval time_arrival;//Arrival time
     struct timeval time_end;    //End time
 
     struct CheckJob *check_job; //If this job is a CheckJob, this holds info
     struct TransJob *trans_job; //If this job is a TransJob, this holds info
+    struct TailNode *tail_node; //If this job is the TailNode, points to prev job
 
     struct Job *next;           //Pointer to the next request
 };
@@ -38,6 +41,7 @@ struct Job {
 struct Queue {
     struct Job *head;
     struct Job *tail;
+    int num_jobs;
 };
 
 
@@ -46,11 +50,6 @@ struct CheckJob newCheckJob(int account_ID);
 struct TransJob newTransJob();
 int addTransToJob(struct TransJob transJob, struct Trans transaction);
 struct Trans newTransaction(int account_ID, int amount);
-
-
-
-
-
 
 
 struct Queue *QUEUE;
@@ -63,44 +62,24 @@ int main(int argc, char *argv[])
     //Initialize queue
     QUEUE = (struct Queue*)malloc(sizeof(struct Queue));
 
-    //Create the head node
+    //Create the tail job
     struct Job tailJob = newJob(TAILNODE);
-
-    //Add it to the queue
-    QUEUE->head = &tailJob;
-    QUEUE->tail = &tailJob;
 
     struct Job job1 = newJob(CHECKJOB);
     struct timeval time = job1.time_arrival;
-    printf("time is: %ld.%6.ld\n", time.tv_sec, time.tv_usec);
 
-    QUEUE->head->next = &job1;
-
-
+    printf("time is: %ld.%06.ld\n", time.tv_sec, time.tv_usec);
 
     struct CheckJob checkJob1 = newCheckJob(3);
     job1.check_job = &checkJob1;
 
+    struct TailNode tail = {
+        .prev = &job1
+    };
+
+    printf("EEEEE: %d\n", tail.prev->request_ID);
 }
 
-
-
-
-int isQueueEmpty(){
-    return (QUEUE->head->next == NULL);
-}
-
-int addToQueue(struct Job job){
-    job.next = QUEUE->tail;
-    QUEUE->tail = &job;
-    return 1;
-}
-struct Job popQueue(){
-    struct Job temp = NULL;
-    temp = (QUEUE->head);
-    QUEUE->head = QUEUE->head.next;
-    return temp;
-}
 
 
 
