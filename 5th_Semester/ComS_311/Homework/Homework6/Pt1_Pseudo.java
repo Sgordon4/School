8a3b5247ce622245d40dd2b9c21cd516d476472e
Sgordon4 <SeanGordonkh@gmail.com>
2019-12-07 18:26:54 -0600

    Completed ComS 311 HW6, CprE 394 Website, and added/rearranged some 4th semester stuff

--- 5th_Semester/ComS_311/Homework/Homework6/Pt1_Pseudo.java
+++ 5th_Semester/ComS_311/Homework/Homework6/Pt1_Pseudo.java
@@ -72,11 +72,14 @@ combo = min( from(i = 0->n) {combo(set, remaining - set[i], list)} )
 int[] comboIter(set, W){
 
     //Store sub problem answers here
-    int[] arr = new int[W+1]
-    *Assign each index in arr to infinity
+    int[] sub = new int[W+1]
+    *Assign each index in sub to infinity
+
+    //Store the indices of the num used for each sum here
+    int[] indices = new int[W+1]
 
     //Sum of 0 will always be an empty set
-    arr[0] = 0
+    sub[0] = 0
 
     for(int i = 1; i < W; i++){
         for(int j = 0; j < set.length; j++){
@@ -84,10 +87,37 @@ int[] comboIter(set, W){
 
             //If the current number can fit
             if(num <= i){
-                //Test if this will add less to
-                num
+                //Replace current answer if it works better
+                int current = sub[i]
+                int new = sub[i - num] + 1
+
+                if(new < current){
+                    //Update our records
+                    sub[i] = new
+                    //Save the index of the number that was used
+                    indices[i] = j
+                }
             }
         }
     }
 
+    //If the last cell is unused, there is no answer
+    if(sub[W] == infinity) return []
+
+    //Otherwise use the stored indices to build a list of [w1, ..., wn]
+    int[] w = new int[set.length]
+    *Assign each index in w to 0
+
+    //Backtrack through our lists
+    int i = W
+    while(i > 0){
+        //Increment the use count (w) of each number used in the end
+        int index = indices[i]
+        w[index]++
+
+        //Go to the number used before this one
+        i -= set[index]
+    }
+
+    return w
 }
