6f96a050c38be50a92bb076e2770308dedd6d966
Sgordon4 <SeanGordonkh@gmail.com>
2020-02-11 01:09:17 -0600

    Mysql interfaces suck ass

---
+++ 6th_Semester/ComS_363/Homework/Project1A/CreateTables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE students (
+    snum integer,
+    ssn integer,
+    name varchar(10),
+    gender varchar(1),
+    dob datetime,
+    c_addr varchar(20),
+    c_phone varchar(10),
+    p_addr varchar(20),
+    p_phone varchar(10),
+    primary key (ssn),
+    
+);
